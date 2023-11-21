02deb4ef6328a1df01baeea53cc5474395da1e77
rickshawwalis <rickshawwallis@gmail.com>
2023-11-21 07:41:48 -0300

    D

--- Script-MudarPagina/script-MudarPagina.js
+++ Script-MudarPagina/script-MudarPagina.js
@@ -15,10 +15,10 @@ function selecionarProduto() {
 
         // Cria condições para cada ID existente
         if (produtoSelecionado.id === "copo300") {
-            window.location.href = "/copo300/ACOMPANHAMENTO/pagina-Acompanhamento.html";
+            window.location.href = "../copo300/ACOMPANHAMENTO/pagina-Acompanhamento.html";
         }
         else if (produtoSelecionado.id === "copo500") {
-            window.location.href = "/copo300/ACOMPANHAMENTO/pagina-Acompanhamento.html"; // Insira o caminho da página desejada
+            window.location.href = "../../Lider-Acai/copo500/ACOMPANHAMENTO/pagina-Acompanhamento.html"; // Insira o caminho da página desejada
         }
         else if (produtoSelecionado.id === "pote240") {
             window.location.href = "/copo300/ACOMPANHAMENTO/pagina-Acompanhamento.html"; // Insira o caminho da página desejada
