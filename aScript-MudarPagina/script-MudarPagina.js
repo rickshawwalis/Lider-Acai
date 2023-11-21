c35bf58139f1a80a42c802b47e77e671f264c2e8
rickshawwalis <rickshawwallis@gmail.com>
2023-11-21 14:03:41 -0300

    sd

--- aScript-MudarPagina/script-MudarPagina.js
+++ aScript-MudarPagina/script-MudarPagina.js
@@ -18,13 +18,13 @@
             window.location.href = "../Lider-Acai/copo300/pagina-Acompanhamento.html";
         }
         else if (produtoSelecionado.id === "copo500") {
-            window.location.href = "../Lider-Acai/copo500/pagina-Acompanhamento.html.html"; // Insira o caminho da página desejada
+            window.location.href = "../../Lider-Acai/copo500/pagina-Acompanhamento.html"; // Insira o caminho da página desejada
         }
         else if (produtoSelecionado.id === "pote240") {
             window.location.href = "../Lider-Acai/pote240/pagina-Acompanhamento.html"; // Insira o caminho da página desejada
         }
         else if (produtoSelecionado.id === "pote360") {
-            window.location.href = "../Lider-Acai/Lider-Acai/pote360/pagina-Acompanhamento.html"; // Insira o caminho da página desejada
+            window.location.href = "../../Lider-Acai/pote360/pagina-Acompanhamento.html"; // Insira o caminho da página desejada
         }
         else if (produtoSelecionado.id === "pote480") {
             window.location.href = "../Lider-Acai/pote480/pagina-Acompanhamento.html"; // Insira o caminho da página desejada
