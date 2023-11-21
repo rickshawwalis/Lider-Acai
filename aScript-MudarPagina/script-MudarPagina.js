c6ad14f418ac4f3ecfcb14f5602cbb5a65d0a458
rickshawwalis <rickshawwallis@gmail.com>
2023-11-21 12:51:16 -0300

    ss

--- aScript-MudarPagina/script-MudarPagina.js
+++ aScript-MudarPagina/script-MudarPagina.js
@@ -57,22 +57,22 @@ function selecionarProduto() {
 
         // Cria condições para cada ID existente
         if (produtoSelecionado.id === "copo300") {
-            window.location.href = "/Lider-Acai/copo300/pagina-Acompanhamento.html";
+            window.location.href = "/copo300/pagina-Acompanhamento.html";
         }
         else if (produtoSelecionado.id === "copo500") {
-            window.location.href = "../../Lider-Acai/copo500/pagina-Acompanhamento.html"; // Insira o caminho da página desejada
+            window.location.href = "../copo500/pagina-Acompanhamento.html.html"; // Insira o caminho da página desejada
         }
         else if (produtoSelecionado.id === "pote240") {
-            window.location.href = "../../Lider-Acai/pote240/pagina-Acompanhamento.html"; // Insira o caminho da página desejada
+            window.location.href = "../Lider-Acai/pote240/pagina-Acompanhamento.html"; // Insira o caminho da página desejada
         }
         else if (produtoSelecionado.id === "pote360") {
-            window.location.href = "../../Lider-Acai/pote360/pagina-Acompanhamento.html"; // Insira o caminho da página desejada
+            window.location.href = "./Lider-Acai/pote360/pagina-Acompanhamento.html"; // Insira o caminho da página desejada
         }
         else if (produtoSelecionado.id === "pote480") {
-            window.location.href = "../../Lider-Acai/pote480/pagina-Acompanhamento.html"; // Insira o caminho da página desejada
+            window.location.href = "./../pote480/pagina-Acompanhamento.html"; // Insira o caminho da página desejada
         }
         else if (produtoSelecionado.id === "marmita500") {
-            window.location.href = "../../Lider-Acai/marmita500/pagina-Acompanhamento.html"; // Insira o caminho da página desejada
+            window.location.href = "./../Lider-Acai/marmita500/pagina-Acompanhamento.html"; // Insira o caminho da página desejada
         }
         else {
             alert("ID do produto não reconhecido");
