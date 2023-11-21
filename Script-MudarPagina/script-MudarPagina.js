e88770b418bd77238155c167b11808c1fe219265
rickshawwalis <rickshawwallis@gmail.com>
2023-11-21 08:28:45 -0300

    g

--- Script-MudarPagina/script-MudarPagina.js
+++ Script-MudarPagina/script-MudarPagina.js
@@ -39,4 +39,42 @@ function selecionarProduto() {
         // Caso nenhum produto tenha sido selecionado, exiba uma mensagem ou faça algo apropriado
         alert("Por favor, selecione um produto.");
     }
+
+    /*
+       if (produtoSelecionado) {
+        // Obtem os valores
+        let dataText = produtoSelecionado.getAttribute('data-text'); // Obtém o valor de data-text
+        let valorProduto = parseFloat(produtoSelecionado.value); // Obtém o valor do produto
+
+        // Armazena os dados no sessionStorage
+        sessionStorage.setItem('dataText', dataText);
+        sessionStorage.setItem('valorProduto', valorProduto);
+
+        // Cria condições para cada ID existente
+        if (produtoSelecionado.id === "copo300") {
+            window.location.href = "../../Lider-Acai/copo300/ACOMPANHAMENTO/pagina-Acompanhamento.html";
+        }
+        else if (produtoSelecionado.id === "copo500") {
+            window.location.href = "../../Lider-Acai/copo500/ACOMPANHAMENTO/pagina-Acompanhamento.html"; // Insira o caminho da página desejada
+        }
+        else if (produtoSelecionado.id === "pote240") {
+            window.location.href = "../../Lider-Acai/pote240/ACOMPANHAMENTO/pagina-Acompanhamento.html"; // Insira o caminho da página desejada
+        }
+        else if (produtoSelecionado.id === "pote360") {
+            window.location.href = "../../Lider-Acai/pote360/ACOMPANHAMENTO/pagina-Acompanhamento.html"; // Insira o caminho da página desejada
+        }
+        else if (produtoSelecionado.id === "pote480") {
+            window.location.href = "../../Lider-Acai/pote480/ACOMPANHAMENTO/pagina-Acompanhamento.html"; // Insira o caminho da página desejada
+        }
+        else if (produtoSelecionado.id === "marmita500") {
+            window.location.href = "../../Lider-Acai/marmita500/ACOMPANHAMENTO/pagina-Acompanhamento.html"; // Insira o caminho da página desejada
+        }
+        else {
+            alert("ID do produto não reconhecido");
+        }
+    } else {
+        // Caso nenhum produto tenha sido selecionado, exiba uma mensagem ou faça algo apropriado
+        alert("Por favor, selecione um produto.");
+    }
+    */
 }
