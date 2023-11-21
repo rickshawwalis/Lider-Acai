de9808ca13e229f164075ba66a19ad0357212cac
rickshawwalis <rickshawwallis@gmail.com>
2023-11-21 08:35:47 -0300

    x

--- Script-MudarPagina/script-MudarPagina.js
+++ Script-MudarPagina/script-MudarPagina.js
@@ -15,7 +15,7 @@ function selecionarProduto() {
 
         // Cria condições para cada ID existente
         if (produtoSelecionado.id === "copo300") {
-            window.location.href = "../copo300/ACOMPANHAMENTO/pagina-Acompanhamento.html";
+            window.location.href = "../Lider-Acai/copo300/ACOMPANHAMENTO/pagina-Acompanhamento.html";
         }
         else if (produtoSelecionado.id === "copo500") {
             window.location.href = "../../Lider-Acai/copo500/ACOMPANHAMENTO/pagina-Acompanhamento.html"; // Insira o caminho da página desejada
