70540c48ef9b05fa0875f23fd9d2cf7fbba6edc4
rickshawwalis <rickshawwallis@gmail.com>
2023-11-21 21:52:11 -0300

    s

--- aENDERECO/script-Endereco.js
+++ aENDERECO/script-Endereco.js
@@ -5,19 +5,21 @@ const FormEnd = () => {
     let cep = document.getElementById('cep').value;
     let cidade = document.getElementById('cidade').value;
     let bairro = document.getElementById('bairro').value;
+    let referencia  = document.getElementById('referencia').value;
 
-    if (nomeRua && numeroCasa && cep && cidade && bairro) {
+    if (nomeRua && numeroCasa && cep && cidade && bairro && referencia) {
         let endereco = {
             nomeRua: nomeRua,
             numeroCasa: numeroCasa,
             cep: cep,
             cidade: cidade,
             bairro: bairro,
+            referencia: referencia,
         };
 
         sessionStorage.setItem('endereco', JSON.stringify(endereco));
 
-        window.location.href = '../aRESUMO/pagina-Resumo.html';
+        window.location.href = '../../Lider-Acai/aRESUMO/pagina-Resumo.html';
     } else {
         alert('Por favor, preencha todos os campos do Endereço para Entrega.');
         return false; // Impede o envio do formulário se a validação falhar
